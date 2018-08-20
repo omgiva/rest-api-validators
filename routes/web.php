780a70355845470b3808a115eb100975f462cb86
Shyam Somanadh <shyam@frontiernxt.com>
2018-08-20 16:04:21 +0530

    test commit to fix issues with git

--- routes/web.php
+++ routes/web.php
@@ -9,36 +9,32 @@
 | It is a breeze. Simply tell Lumen the URIs it should respond to
 | and give it the Closure to call when that URI is requested.
 |
-*/
+ */
 
 $router->get('/', function () use ($router) {
-    return $router->app->version();
+	return $router->app->version();
 });
 
 $router->get('/m', function () use ($router) {
-    return 'validated';
+	return 'validated';
 });
-
 
 $api = app('Dingo\Api\Routing\Router');
 
-
-
 $api->version('v1', function ($api) {
 
-    //var_export($api);
+	//var_export($api);
 
-    /*$api->get('test', function () {
-        return 'It is ok';
-    });*/
+	/*$api->get('test', function () {
+		        return 'It is ok';
+	*/
 
-
-    $api->get('validate/phone/{number}', ['middleware' => 'cors', 'uses' =>'App\Http\Controllers\Api\ValidatorController@phone']);
-
+	$api->get('validate/phone/{number}', ['middleware' => 'cors', 'uses' => 'App\Http\Controllers\Api\ValidatorController@phone']);
 
 });
 
+//adding a comment
 
 /*$router->get('profile', [
-    'as' => 'profile', 'uses' => 'UserController@showProfile'
+'as' => 'profile', 'uses' => 'UserController@showProfile'
 ]);*/
